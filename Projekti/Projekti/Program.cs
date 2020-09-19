8c109a4cc358d9886f5043ff6b5fc8ae6e6ab39a
antti19202 <60737604+antti19202@users.noreply.github.com>
2020-09-19 18:57:38 +0300

    Päävalikko luotu uuteen classiin

---
+++ Projekti/Projekti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uusi class Päävalikko
+            Päävalikko päävalikko = new Päävalikko();
+
+            // Käynnistää päävalikon
+            päävalikko.Aloitusvalikko();
+        }
+    }
+}
